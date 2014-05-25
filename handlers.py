6b0f21f6ddb7e22f83dedaafe86a30a00a4bded4
Jace <lampsidewebdesign@gmail.com>
2014-05-25 00:05:30 -0500

    basic-fied starter pages app.yaml and handlers.py

--- handlers.py
+++ handlers.py
@@ -24,6 +24,6 @@ class ContactHandler(webapp2.RequestHandler):
 		message = cgi.escape(self.request.get("message"))
 		email = "Sender: " + sender + "\n" + "Full name: " + fullname + "\n" + "Message: " + "\n" + message
 		
-		To = "lampsidewebdesign@gmail.com" # client email
-		From = "lampsidewebdesign@gmail.com" # admin email
+		To = "CLIENT_EMAIL"
+		From = "ADMIN_EMAIL"
 		mail.send_mail(From, To, subject, email)
